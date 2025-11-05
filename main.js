e5567225180600d022e92735a5894fc8b2ec88a2
leolinder-hash <leolinder074@gmail.com>
2025-11-05 16:57:38 +0100

    Added JS-functionality for clicking hamburger/close-buttons

---
+++ main.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".header__hamburger");
+const menu = document.querySelector(".header__menu");
+const closeButton = document.querySelector(".header__close");
+
+hamburger.addEventListener("click", ()=>menuPopUp());
+closeButton.addEventListener("click", ()=>closeMenu());
+
+function menuPopUp(){
+  document.body.classList.toggle("menu-open");
+}
+
+function closeMenu(){
+  document.body.classList.remove("menu-open");
+}
